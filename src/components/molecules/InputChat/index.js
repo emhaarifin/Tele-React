377f3ef2e507d4a79d3a66c4a9913a5799b4e843
emhaarifin <101muhammadarifin@gmail.com>
2021-09-14 17:54:15 +0700

    feat: update styling for chatting room

--- src/components/molecules/InputChat/index.js
+++ src/components/molecules/InputChat/index.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React from 'react';
 
-import Input from "@components/atoms/Input";
-import "./styles.scss";
-import { cameraIcon, plusIcon, stickerIcon } from "../../../assets/images";
+import Input from '@components/atoms/Input';
+import './styles.scss';
+import { cameraIcon, plusIcon, stickerIcon } from '../../../assets/images';
 function Index({ onChange, onSubmit, value, ...props }) {
   return (
-    <div className='input-msg background--white flex flex--align-center flex--justify-space-between'>
-      <div className='input-msg--content width--100'>
+    <div className="input-msg background--white flex flex--align-center flex--justify-space-between">
+      <div className="input-msg--content flex width--100">
         <Input
-          id='send-msg'
-          placeholder='Type your message...'
+          id="send-msg"
+          placeholder="Type your message..."
           value={value}
           onSubmit={onSubmit}
-          className='width--100  border-radius--15 background--quill-gray input-msg--chat'
+          className="width--100  border-radius--15 background--quill-gray input-msg--chat"
           onChange={onChange}
           {...props}
         ></Input>
-        <div className='input-msg--icon flex flex--align-center '>
+        <div className="input-msg--icon flex flex--align-center ">
           <div>
-            <img src={plusIcon} alt='plus'></img>
+            <img src={plusIcon} alt="plus"></img>
           </div>
           <div>
-            <img src={stickerIcon} alt='sticker'></img>
+            <img src={stickerIcon} alt="sticker"></img>
           </div>
           <div>
-            <img src={cameraIcon} alt='camera'></img>
+            <img src={cameraIcon} alt="camera"></img>
           </div>
         </div>
       </div>
