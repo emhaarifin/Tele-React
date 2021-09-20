50dd81683673318264c3da55e283a02dbe7f9a09
emhaarifin <101muhammadarifin@gmail.com>
2021-09-20 20:53:56 +0700

    disable eslint

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react';
 import Chatting from '@components/organism/Chatting';
 import Sidebar from '@components/organism/SidebarLeft';
